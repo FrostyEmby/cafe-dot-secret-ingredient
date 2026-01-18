395579f82e9159d61ba6dbb6f2b97a481a2e6af7
Emby <frostyemby@gmail.com>
2026-01-18 09:58:19 -0600

    Mini Menu
    Put the mini menu in all scenes, and made all of its buttons work. Makes it easy to navigate between everything

---
+++ scenes/mini_menu.gd
@@ -0,0 +1,21 @@
+extends Panel
+
+
+func _on_home_pressed() -> void:
+	get_tree().change_scene_to_file("uid://dhs6v4k8dmjo4")
+
+
+func _on_incubator_pressed() -> void:
+	get_tree().change_scene_to_file("uid://csy6oy6sctiow")
+
+
+func _on_terrarium_pressed() -> void:
+	get_tree().change_scene_to_file("uid://77pmhayrj8nl")
+
+
+func _on_bestiary_pressed() -> void:
+	get_tree().change_scene_to_file("uid://puey23wcodsh")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("uid://bgagifdev40yk")
