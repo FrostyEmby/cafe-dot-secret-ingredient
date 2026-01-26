83be89696eca36fe0cead1390172e052356c8591
Emby <frostyemby@gmail.com>
2026-01-26 16:36:08 -0600

    terrarium backend thingies

--- scripts/save.gd
+++ scripts/save.gd
@@ -11,3 +11,5 @@ class_name Save
 @export var nest1 : NestData = load("res://resources/nest1.tres")
 @export var nest2 : NestData = load("res://resources/nest2.tres")
 @export var nest3 : NestData = load("res://resources/nest3.tres")
+
+# TERRARIUMS
---
+++ scripts/terrarium-data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name TerrariumData
+
+@export var creature1 : EggData = EggData.new()
+@export var creature2 : EggData = EggData.new()
+@export var creature3 : EggData = EggData.new()
+@export var creature4 : EggData = EggData.new()
