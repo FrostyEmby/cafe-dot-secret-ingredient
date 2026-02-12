274a6fe0b0afe3a2efbd139d024cbc2f866d2dcf
Emby <frostyemby@gmail.com>
2026-02-12 06:24:21 -0600

    add beginnings of the diet system

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -2,8 +2,10 @@ extends Sprite2D
 
 signal holding(held_creature : EggData)
 signal releasing(released_creature : EggData)
+signal fed
 
 var active : bool = false
+var gathering : bool = false
 var creature : Egg = Egg.new()
 
 #TODO: make this the hub for matching between the shelf and everything else
@@ -56,13 +58,17 @@ func _outside_press():
 	# waits a small amount of time to make sure release() doesn't start
 	$Removal.start(.1)
 	await $Removal.timeout
-	if active: # removes egg if release() does not get pressed
+	if active and not gathering: # removes egg if release() does not get pressed
 		$Grip.frame = 0
 		texture = null
 		active = false
 		$Tip.text = ""
 		creature.change_visibility(true) # since creature is copied, makes it visible where it came from
-
+	elif active and gathering:
+		active = false
+		gathering = false
+		$Grip.frame = 0
+		texture = null
 
 # extra visual for clicks
 func _grip_ungrip():
@@ -73,7 +79,60 @@ func _grip_ungrip():
 		$Grip.frame = 0
 
 
-# TODO: if pressed outside of area like hachery or terrarium, return to initial spot
+func gather(food : CreatureStats.food_pref):
+	# if active or currently gathering, don't proceed
+	if active or gathering:
+		return
+	
+	active = true
+	gathering = true
+	
+	$Grip.frame = 1
+	
+	# changes texture to match the food you take
+	match food:
+		CreatureStats.food_pref.MEAT:
+			texture = load("res://art/eggs/meat.png")
+		CreatureStats.food_pref.PLANTS: 
+			texture = load("res://art/eggs/plant.png")
+		CreatureStats.food_pref.ELEMENTS: 
+			texture = load("res://art/eggs/fish.png")
+		CreatureStats.food_pref.FISH: 
+			texture = load("res://art/eggs/fish.png")
+		CreatureStats.food_pref.INSECTS:
+			texture = load("res://art/eggs/insect.png")
+		_:
+			texture = load("res://art/eggs/corpse.png") # make it easy to spot bugs
+
+
+func feed():
+	active = false
+	gathering = false
+	$Grip.frame = 0
+	texture = null
+	fed.emit()
+
+
+func _on_meat_pressed() -> void:
+	gather(CreatureStats.food_pref.MEAT)
+
+
+func _on_plant_pressed() -> void:
+	gather(CreatureStats.food_pref.PLANTS)
+
+
+func _on_candy_pressed() -> void:
+	gather(CreatureStats.food_pref.SWEETS)
+
+
+func _on_insect_pressed() -> void:
+	gather(CreatureStats.food_pref.INSECTS)
+
+
+func _on_element_pressed() -> void:
+	gather(CreatureStats.food_pref.ELEMENTS)
+	
+	
 func _process(_delta: float) -> void:
 	global_position = get_global_mouse_position()
 	
